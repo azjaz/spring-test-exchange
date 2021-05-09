da38225c9c7872685453f28c106955e2defc8b60
andrey_ziabko <andrey_ziabko@epam.com>
2021-05-09 19:36:28 +0300

    init commit

---
+++ src/main/java/com/forex/microservice/training/springforextraining/SpringForexTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.forex.microservice.training.springforextraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForexTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringForexTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forex/microservice/training/springforextraining/SpringForexTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forex.microservice.training.springforextraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringForexTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
