44d993807a42bd825aeb2a6f24d1c916fe6e97a0
andrey_ziabko <andrey_ziabko@epam.com>
2021-05-09 20:36:35 +0300

    add 2 classes to initial

---
+++ src/main/java/com/forex/microservice/training/springforextraining/ExchangeValueRepository.java
@@ -0,0 +1,7 @@
+package com.forex.microservice.training.springforextraining;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+    ExchangeValue findByFromAndTo(String from, String to);
+}
